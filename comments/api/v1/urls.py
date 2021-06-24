a36734eb89d5e0b5025aac04192960def03ce0a3
Ricardo Graciano <ricardo.mendonca@jungledevs.com>
2021-06-24 10:46:11 -0300

    refactor: Split url files

--- comments/api/v1/urls.py
+++ comments/api/v1/urls.py
@@ -1,23 +1,27 @@
-"""
-API V1: Comments Urls
-"""
-###
-# Libraries
-###
-from django.urls import re_path, include
-from rest_framework_nested import routers
-
-
-###
-# Routers
-###
-""" Main router """
-router = routers.SimpleRouter()
-
-
-###
-# URLs
-###
-urlpatterns = [
-    re_path(r'^', include(router.urls)),
-]
+"""
+API V1: Comments Urls
+"""
+###
+# Libraries
+###
+from .views import CommentViewSet
+
+from django.urls import re_path, include
+from posts.api.v1.urls import router as posts_router 
+from rest_framework_nested import routers
+
+
+###
+# Routers
+###
+""" Main router """
+router = routers.NestedSimpleRouter(posts_router, r'posts', lookup='post')
+router.register(r'comments', CommentViewSet, 'topics-posts-comments')
+
+
+###
+# URLs
+###
+urlpatterns = [
+    re_path(r'^', include(router.urls)),
+]
--- posts/api/v1/urls.py
+++ posts/api/v1/urls.py
@@ -1,23 +1,27 @@
-"""
-API V1: Posts Urls
-"""
-###
-# Libraries
-###
-from django.urls import re_path, include
-from rest_framework_nested import routers
-
-
-###
-# Routers
-###
-""" Main router """
-router = routers.SimpleRouter()
-
-
-###
-# URLs
-###
-urlpatterns = [
-    re_path(r'^', include(router.urls)),
-]
+"""
+API V1: Posts Urls
+"""
+###
+# Libraries
+###
+from .views import PostViewSet
+
+from django.urls import re_path, include
+from rest_framework_nested import routers
+from topics.api.v1.urls import router as topics_router
+
+
+###
+# Routers
+###
+""" Main router """
+router = routers.NestedSimpleRouter(topics_router, r'topics', lookup='topic')
+router.register(r'posts', PostViewSet, basename='topics-posts')
+
+
+###
+# URLs
+###
+urlpatterns = [
+    re_path(r'^', include(router.urls)),
+]
